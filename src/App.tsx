b6c16fd9090e192d94783ac2b71813e0f4cb189f
Gabor Dandar <dandarg@gmail.com>
2022-11-09 15:11:20 +0100

    added basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
 import "./App.css";
 import PropertyList from "./features/properties/PropertyList";
 import PropertySearch from "./features/properties/PropertySearch";
 import PropertySortOptions from "./features/properties/PropertySortOptions";
 
+const layout = {
+  main: css({
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+    maxWidth: "100vw",
+    margin: 0,
+  }),
+  header: css({
+    height: 72,
+  }),
+  content: css({
+    display: "flex",
+    maxWidth: "100vw",
+    maxHeight: "calc(100vh - 72px)",
+    "& img": { width: "100%", height: "100%", objectFit: "cover" },
+  }),
+  rightPane: css({
+    overflow: "auto",
+    width: 680,
+    flex: "0 0 auto",
+  }),
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div css={layout.main}>
+      <header css={layout.header}>
         <PropertySearch />
-        <PropertySortOptions />
-        <PropertyList />
       </header>
+      <div css={layout.content}>
+        <div>
+          <img src="/map.jpg" />
+        </div>
+        <div css={layout.rightPane}>
+          <PropertySortOptions />
+          <PropertyList />
+        </div>
+      </div>
     </div>
   );
 }
--- src/features/properties/PropertyCard.tsx
+++ src/features/properties/PropertyCard.tsx
@@ -17,8 +17,6 @@ export default function PropertyCard({ property }: Props) {
       <div>{`${property.units[0].bedroom}bd, ${property.units[0].bathroom}ba, ${property.units[0].squareFootage}ft2`}</div>
       <div>{`${property.address.streetNumber} ${property.address.route}`}</div>
       <div>{`${property.address.locality}, ${property.address.stateCode}`}</div>
-      <div>{property.status}</div>
-      <div>{`${new Date(property.createdAt)}`}</div>
     </div>
   );
 }
